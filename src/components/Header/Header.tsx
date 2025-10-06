6049b42486f8b2142acf5f717b2669e62e15feb0
Даниил <dan-555@list.ru>
2025-10-06 18:16:14 +0300

    fixed the logo display issue on Vercel

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import HeaderTop from "./HeaderTop/HeaderTop.tsx";
 function Header() {
 	return (
 		<HeaderStyle>
-				<a href="#"><LogoOzon src="public/Header/Nav/Logo.jpg" alt="logo-ozon" /></a>
+				<a href="#"><LogoOzon src="/Header/Nav/Logo.jpg" alt="logo-ozon" /></a>
 				<HeaderTop/>
 				
 		</HeaderStyle>
