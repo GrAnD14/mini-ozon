4728d66d7c96f2b9fdb030659ddd92e3114f7b1c
Ivan <ivanmakarevic3@gmail.com>
2025-10-06 11:12:45 +0300

    fix: low bags

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import HeaderTop from "./HeaderTop/HeaderTop.tsx";
 function Header() {
 	return (
 		<HeaderStyle>
-				<a href="#"><LogoOzon src="public/Header/Nav/Logo.jpg" alt="logo-ozon" /></a>
+				<a href="#"><img src="public/Header/Nav/Logo.jpg" alt="logo-ozon" /></a>
 				<HeaderTop/>
 		</HeaderStyle>
 	);
