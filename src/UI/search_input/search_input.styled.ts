c00274f35b948dfa6b9ee10eabc9bec12f7869e9
Ivan Makarevich <60136217+Netron-Frontend@users.noreply.github.com>
2025-10-07 00:31:33 +0300

    Frontend (#12)
    * feat: add Footer component
    
    * chore: restore Footer
    
    * Revert "added a logo, search, and icons"
    
    This reverts commit f6ba37c6aa21961022c2cdf00990af5a37acc29b.
    
    ---------
    
    Co-authored-by: Diana <Shimlykhdiana@gmail.com>

--- src/UI/search_input/search_input.styled.ts
+++ src/UI/search_input/search_input.styled.ts
@@ -4,75 +4,71 @@ export const StyledSearchInput = styled.div` //экспортируем пере
 		//Пример: экспортируем константу Имя_переменной = стилизация.Элемент (может быть button, если кнопка. div, если блочный элемент какой-то и т.д. )
 		//(img (для стилизации фото), h1-h6(для стилизации заголовков), p(для стилизации параграфов) и т.п.)
 		
-		//Дальше просто пишем, как обычные стили css
-.search-container {
-	position: sticky;
-	display: flex;
+		//Дальше просто пишем, как обычные стили css без указывания классов и скобок (1-й переменной = 1 стилизация. Новая переменная = новая стилизация)
+		display: flex;
   	align-items: center;
+		justify-content: space-between;
   	border: 2px solid rgba(0, 91, 255, 1);
   	border-radius: 10px;
   	width: 664px;
   	height: 47px;
-  	background: rgba(0, 91, 255, 1);
-  	padding-left: 6px; 
-	padding-right: 40px; 
-}
-
-.category-btn {
- 	margin-right: 8px; 
+  	background: none;
+`
+export const CategoryBtn = styled.button`
+	margin-right: 8px;
 	border-radius: 6px;
 	background: rgba(245, 245, 245, 1);
-	width: 100.68px;
+	width: 150px;
 	height: 37.21px;
 	color: rgba(102, 102, 102, 1);
-	font-family: Inter;
+	font-family: 'Inter', sans-serif;
 	font-size: 14px;
 	font-weight: 400;
 	line-height: 17px;
-}
+`
 
-.search-input {
-  	padding-left: 8px;  
-	width: 559.05px;
+export const SearchInputBlock = styled.input`
+	padding-left: 8px;
+	width: 100%;
 	height: 43.08px;
 	background: rgba(255, 255, 255, 1);
 	border-radius: 10px;
 	color: rgba(125, 125, 125, 1);
-	font-family: Inter;
+	font-family: 'Inter', sans-serif;
 	font-size: 14px;
 	font-weight: 400;
 	line-height: 17px
-}
+`
 
-.icon-camera-link {
+export const IconCameraLink = styled.a`
   	display: inline-block;
-}
+`
+export const IconCamera = styled.img`
+  	margin-right: 10px;
+		transform: translate(-50px, 0px);
+		cursor: pointer;
+		&:hover {
+        content: url("/Header/Nav/Camera_hover.svg");
+    }
+`
 
-.icon-camera {
-  	margin-right: 10px; 
-	transform: translate(-50px, 0px);
-	cursor: pointer;
-}
-
-.icon-camera:hover {
-	content: url("/Header/Nav/Camera_hover.svg"); 
-}
-
-.search-btn-link {
-	width: 80px;
-	height: 45px;
-	border-radius: 10px;
-}
-
-.search-btn-link:hover {
-	background: rgba(198, 216, 251, 0.2);
-}
-
-.search-btn {
+export const SearchBtnLink = styled.a`
+    width: 100px;
+    height: 45px;
+    border-radius: 6px;
+    background: rgba(0, 91, 255, 1);
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin: 0 auto;
+    &:hover {
+        background: rgb(96, 146, 229);
+    }
+`
+export const SearchBtn = styled.img`
 	width: 24.34px;
 	height: 45px;
 	border-radius: 10px;
 	cursor: pointer;
-}
-
-`;
+    margin: 0 auto;
+	`
