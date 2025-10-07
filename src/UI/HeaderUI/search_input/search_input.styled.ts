b1d32c76f061a538f9ed492863c252a6a77647df
Ivan <ivanmakarevic3@gmail.com>
2025-10-07 11:58:08 +0300

    refactor: was refactored old code

---
+++ src/UI/HeaderUI/search_input/search_input.styled.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+
+export const StyledSearchInput = styled.div `
+	position: sticky;
+	display: flex;
+	align-items: center;
+	border: 2px solid rgba(0, 91, 255, 1);
+	border-radius: 10px;
+	width: 664px;
+	height: 47px;
+	background: rgba(0, 91, 255, 1);
+	padding-left: 6px;
+	padding-right: 40px;
+`
+
+export const CategoryBtn = styled.button `
+
+ 	margin-right: 8px;
+	border-radius: 6px;
+	background: rgba(245, 245, 245, 1);
+	width: 100.68px;
+	height: 37.21px;
+	color: rgba(102, 102, 102, 1);
+	font-family: 'Inter', sans-serif;
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 17px;
+`
+
+export const SearchInputSpace = styled.input `
+  	padding-left: 8px;
+		width: 559.05px;
+		height: 43.08px;
+		background: rgba(255, 255, 255, 1);
+		border-radius: 10px;
+		color: rgba(125, 125, 125, 1);
+		font-family: 'Inter', sans-serif;
+		font-size: 14px;
+		font-weight: 400;
+		line-height: 17px
+`
+
+export const IconCameraLink = styled.div `
+  	display: inline-block;
+`
+
+export const IconCamera = styled.img `
+  	margin-right: 10px;
+	transform: translate(-50px, 0px);
+	cursor: pointer;
+		&:hover {
+        content: url("/Header/Nav/Camera_hover.svg");
+		}
+`
+
+export const SearchBtnLink = styled.button `
+	width: 80px;
+	height: 45px;
+	border-radius: 10px;
+		&:hover {
+        background: rgba(198, 216, 251, 0.2);
+		}
+`
+
+export const SearchBtn = styled.img `
+	width: 24.34px;
+	height: 45px;
+	border-radius: 10px;
+	cursor: pointer;
+`
