b40b8fd6b23ff564974108847f21d18cae06a4c6
Ivan Makarevich <60136217+Netron-Frontend@users.noreply.github.com>
2025-10-08 22:27:30 +0300

    Frontend (#21)
    * refactor: was refactored old code (#16)
    
    * feat: re-add marquee, header links, and header height and search update
    
    * fix: add spacing for HeaderLinks, hover effect for HeaderCatalogButton, and correct Ozon logo path
    
    * fix: adjust font styling in header links
    
    * feat: add Footer component
    
    * chore: restore Footer
    
    * Header low fix (#18)
    
    * refactor: Dianas code.
    
    * feauture: marketplace cards (work in progress)
    
    * feauture: was made a swiper with names and jobs of team (#20)
    
    ---------
    
    Co-authored-by: Даниил <dan-555@list.ru>
    Co-authored-by: Diana <Shimlykhdiana@gmail.com>

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,11 @@
 import {MainStyled} from "./Main.styled.ts";
 import ProductCard from "../../UI/MainUI/ProductCard/ ProductCard.tsx";
+import Slider from "./Slider/Slider.tsx";
 
 const Main = () => {
 	return (
 		<MainStyled>
+			<Slider/>
 			<ProductCard id={""} title={""} currentPrice={0} originalPrice={0} discount={0} rating={0} reviewsCount={0} />
 		</MainStyled>
 	);
---
+++ src/components/Main/Slider/Slider.tsx
@@ -0,0 +1,34 @@
+// import required modules
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay, Navigation} from 'swiper/modules';
+
+import './slider.css'
+
+
+// // Import Swiper styles
+// @ts-ignore
+import 'swiper/css';
+// @ts-ignore
+import "swiper/css/navigation";
+
+const Slider = () => {
+	
+	
+	return (
+		<Swiper
+			spaceBetween={50}
+			centeredSlides={true}
+			autoplay={{delay: 5000, disableOnInteraction: false,}}
+			navigation={true}
+			modules={[Autoplay, Navigation]}
+			style={{marginBottom: '65px'}}
+		>
+			<SwiperSlide><img src="/Main/Slider/Slide_1.png" alt="slide_1"/></SwiperSlide>
+			<SwiperSlide><img src="/Main/Slider/Slide_2.png" alt="slide_2"/></SwiperSlide>
+			<SwiperSlide><img src="/Main/Slider/Slide_3.png" alt="slide_3"/></SwiperSlide>
+			<SwiperSlide><img src="/Main/Slider/Slide_4.png" alt="slide_4"/></SwiperSlide>
+		</Swiper>
+	);
+};
+
+export default Slider;
