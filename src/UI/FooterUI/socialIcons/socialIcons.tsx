03f0f3d7e1563f8aa91208d3f44dc51627bb6e7f
Ivan <ivanmakarevic3@gmail.com>
2025-10-09 12:25:54 +0300

    feauture: Added prettier, configured husky, added a type for image imports, and changed them in the project

--- src/UI/FooterUI/socialIcons/socialIcons.tsx
+++ src/UI/FooterUI/socialIcons/socialIcons.tsx
@@ -1,19 +1,20 @@
-import { SocialCenter, SocialLink } from "./socialIcons.styled";
+import { SocialCenter, SocialLink } from './socialIcons.styled';
+import ImagesImports from '../../../Imports/ImagesImports.ts';
 
 const SocialIcons = () => {
-  return (
-    <SocialCenter>
-      <SocialLink href="https://vk.com/ozon/?perehod=footer">
-        <img src="/Footer/media/vk.svg" alt="vk" />
-      </SocialLink>
-      <SocialLink href="https://ok.ru/ozon/?perehod=footer">
-        <img src="/Footer/media/ok.svg" alt="ok" />
-      </SocialLink>
-      <SocialLink href="https://t.me/ozonru/?perehod=footer">
-        <img src="/Footer/media/tg.svg" alt="telegram" />
-      </SocialLink>
-    </SocialCenter>
-  );
+	return (
+		<SocialCenter>
+			<SocialLink href='https://vk.com/ozon/?perehod=footer'>
+				<img src={ImagesImports.vk} alt='vk' />
+			</SocialLink>
+			<SocialLink href='https://ok.ru/ozon/?perehod=footer'>
+				<img src={ImagesImports.ok} alt='ok' />
+			</SocialLink>
+			<SocialLink href='https://t.me/ozonru/?perehod=footer'>
+				<img src={ImagesImports.tg} alt='telegram' />
+			</SocialLink>
+		</SocialCenter>
+	);
 };
 
 export default SocialIcons;
