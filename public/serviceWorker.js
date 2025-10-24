a5384c4b11a8b99b25c09c8f61b52496d18a7970
Даниил <dan-555@list.ru>
2025-10-25 00:09:15 +0300

    fix(): paths to imgs

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,13 @@
 self.addEventListener('install', () => {
 	console.log('Service Worker установлен');
+	self.skipWaiting();
 });
 
-self.addEventListener('fetch', () => {
-	// Заглушка — без кэша пока
+self.addEventListener('activate', () => {
+	console.log('Service Worker активирован');
+});
+
+self.addEventListener('fetch', event => {
+	// Пропускаем всё, но указываем, что SW обрабатывает запросы
+	event.respondWith(fetch(event.request));
 });
