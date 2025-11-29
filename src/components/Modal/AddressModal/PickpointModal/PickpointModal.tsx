4faa71005e2291b5b20bcf4cdd6bb32ac69c15fe
Даниил <dan-555@list.ru>
2025-11-29 19:37:56 +0300

    Apply changes from commit 87dd209

--- src/components/Modal/AddressModal/PickpointModal/PickpointModal.tsx
+++ src/components/Modal/AddressModal/PickpointModal/PickpointModal.tsx
@@ -22,7 +22,8 @@ import L from 'leaflet';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import { Marker } from 'react-leaflet';
+
+import { Marker } from 'react-leaflet/Marker';
 
 // Fix Leaflet default icons
 delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: () => void })._getIconUrl;
